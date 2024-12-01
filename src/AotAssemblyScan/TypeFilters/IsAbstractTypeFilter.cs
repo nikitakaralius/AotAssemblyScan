de6a378deacd173642c9a45255f312b46a7af846
nikitakaralius <nikitakaralyus@gmail.com>
2024-12-01 19:57:46 +0300

    Added code to pass Includes_only_abstract_classes_when_using_IsAbstract test

---
+++ src/AotAssemblyScan/TypeFilters/IsAbstractTypeFilter.cs
@@ -0,0 +1,35 @@
+#pragma warning disable CS0414 // Field is assigned but its value is never used
+
+using Microsoft.CodeAnalysis;
+
+namespace AotAssemblyScan.TypeFilters;
+
+public sealed class IsAbstractTypeFilter : ITypeFilter
+{
+    private const string AttributeTypeName = "IsAbstract";
+    private bool _isAbstract = true;
+
+    public bool TryAdd(AttributeData attribute)
+    {
+        var attributeClass = attribute.AttributeClass;
+
+        if (attributeClass is null)
+            return false;
+
+        if (attributeClass.Name != AttributeTypeName)
+            return false;
+
+        if (attribute.ConstructorArguments.Length == 0)
+            return false;
+
+        if (attribute.ConstructorArguments[0].Value is not bool isAbstract)
+            return false;
+
+        _isAbstract = isAbstract;
+
+        return true;
+    }
+
+    public bool Matches(INamedTypeSymbol type) =>
+        type.IsAbstract == _isAbstract;
+}
